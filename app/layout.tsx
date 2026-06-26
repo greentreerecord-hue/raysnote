ec0e5e17fb38c1518750fc7c83cbe608d61beb79
greentreerecord-hue <greentreerecord@gmail.com>
2026-06-26 14:59:14 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,19 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Ray'sStream",
+  description: "Ray'sStream video website"
+};
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+} 
 
