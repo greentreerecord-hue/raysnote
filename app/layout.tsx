4574f2d3b52902cd26662b0fbbc8bb9811e74c12
greentreerecord-hue <greentreerecord@gmail.com>
2026-06-26 12:51:26 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Ray'snote",
+  description: "Independent online publishing by Ray'snote",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+} 
