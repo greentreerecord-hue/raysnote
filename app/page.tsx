44232b8bd7febcb36464cc04a81a10ec9bccd368
greentreerecord-hue <greentreerecord@gmail.com>
2026-06-26 13:12:38 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "Ray'snote",
+  description: "Ray'snote online publishing",
+};
+
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body>{children}</body>
